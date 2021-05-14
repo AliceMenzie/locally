09a0b3c4b93c80c92a6c533eb0eb9f2bec53d4e9
Alice Menzie <Alice.Menzie@gmail.com>
2021-05-14 17:37:59 +1000

    MODEL: add validations to product model

--- app/models/address.rb
+++ app/models/address.rb
@@ -2,5 +2,5 @@ class Address < ApplicationRecord
   belongs_to :user
   validates :street, :number, :postcode, :state, presence: true
   validates :street, length: {in: 2..50}
-  validates :number, numericality: {greater_than: 0}
+  validates :number, numericality: {only_integer: true, greater_than: 0}
 end
--- app/models/product.rb
+++ app/models/product.rb
@@ -1,3 +1,6 @@
 class Product < ApplicationRecord
   belongs_to :user
+  validates :name, :description, :price, :category, :status, presence: true
+  validates :price, numericality: {only_integer: true, less_than_or_equal_to: 5_000}
+  validates :description, length: {in: 2..500}
 end
