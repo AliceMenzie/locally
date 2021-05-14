c2631f4338e199e743b5b438cb0b9fca55f18019
Alice Menzie <Alice.Menzie@gmail.com>
2021-05-14 17:08:38 +1000

    MODEL: add validations to address model

--- app/models/address.rb
+++ app/models/address.rb
@@ -1,3 +1,6 @@
 class Address < ApplicationRecord
   belongs_to :user
+  validates :street, :number, :postcode, :state, presence: true
+  validates :street, length: {in: 2..50}
+  validates :number, numericality: {greater_than: 0}
 end
